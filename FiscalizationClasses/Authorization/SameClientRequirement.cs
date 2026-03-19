517ebafe934cd814adef78af6546a2c1bf252b50
Popov Danil <PopovDanil123@gmail.com>
2026-03-19 10:35:14 +0600

    Deleted Custom Authorization policy

--- FiscalizationClasses/Authorization/SameClientRequirement.cs
+++
@@ -1,8 +0,0 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-
-//namespace Fiscalizator.FiscalizationClasses.Authorization
-//{
-//    public class SameClientRequirement : IAuthorizationRequirement
-//    {
-//    }
-//}
